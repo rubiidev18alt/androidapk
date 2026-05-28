002432274dbd05906b781f1351eb0e0ab2ae14d8
rubiidev18alt <164731988+rubiidev18alt@users.noreply.github.com>
2026-05-28 01:26:42 -0700

    Build IBM PC 5150 emulator activity

--- app/src/main/java/com/example/simpleapk/MainActivity.kt
+++ app/src/main/java/com/example/simpleapk/MainActivity.kt
@@ -4,8 +4,30 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var emulatorView: EmulatorView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val emulator = IbmPc5150Emulator()
+        emulator.bootFromAssets(assets)
+
+        emulatorView = findViewById(R.id.emulatorView)
+        emulatorView.attach(emulator)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (::emulatorView.isInitialized) {
+            emulatorView.start()
+        }
+    }
+
+    override fun onPause() {
+        if (::emulatorView.isInitialized) {
+            emulatorView.stop()
+        }
+        super.onPause()
     }
 }
